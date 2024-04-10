000b8d1325e205614bd92537178f1b62f643eff3
Harshal.Ogale-at-447515355797 <harshal.ogale@flentas.com>
2024-04-10 13:23:34 +0530

    DB connection

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//   } catch (error) {
+//     console.error("DB ERROR", error);
+//     throw error;
+//   }
+// })();
